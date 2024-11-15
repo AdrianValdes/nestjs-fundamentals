bfebdf89d1fc61d91b788c02c1dfd6934c051a0b
Adrian Valdes <adrianvaldeshdz@gmail.com>
2024-11-15 08:53:30 +0100

    chore: add config module

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,10 +6,25 @@ import { CoffeesService } from "./coffees.service";
 import { Coffee } from "./entities/coffee.entity";
 import { Flavor } from "./entities/flavor.entity";
 
+export class MockCoffeesService {}
+
+const foo = {
+	provide: "COFFEE_BRANDS", // 👈
+	useValue: ["buddy brew", "nescafe"], // array of coffee brands,
+};
+
 @Module({
 	imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
 	controllers: [CoffeesController],
-	providers: [CoffeesService],
+	providers: [
+		{
+			provide: CoffeesService,
+			useClass: CoffeesService,
+			// useFactory: () => ["buddy brew", "nescafe"],
+			// useValue: new MockCoffeesService(), // useValue is used to provide a static value
+		},
+	],
+	// providers: [CoffeesService, foo], // To provide a static value
 	exports: [CoffeesService],
 })
 export class CoffeesModule {}
