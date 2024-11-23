0564e4479b9234d92db8be60b2cab7bfeb280e99
Adrian Valdes <adrianvaldeshdz@gmail.com>
2024-11-23 09:24:48 +0100

    feat: add apitags

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -17,9 +17,10 @@ import { PaginationQueryDto } from "src/common/dto/pagination-query.dto";
 import { Public } from "src/common/decorators/public.decorator";
 import { ParseIntPipe } from "src/common/pipes/parse-int.pipe";
 import { Protocol } from "src/common/decorators/protocol.decorator";
-import { ApiForbiddenResponse } from "@nestjs/swagger";
+import { ApiForbiddenResponse, ApiTags } from "@nestjs/swagger";
 
 // @UsePipes() // apply to all routes in the controller
+@ApiTags("coffees")
 @Controller("coffees")
 export class CoffeesController {
 	constructor(private readonly coffeesService: CoffeesService) {}
